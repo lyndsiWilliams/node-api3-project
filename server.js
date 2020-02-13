efc4d96d9a6d27a8a196763841277bd04078ff40
lyndsiWilliams <kcatgirl@gmail.com>
2020-02-13 07:50:04 -0600

    Server set up, logger middleware implemented

--- server.js
+++ server.js
@@ -2,12 +2,28 @@ const express = require('express');
 
 const server = express();
 
+
+// middleware usage
+server.use(express.json());
+server.use(logger);
+
+
+// routes
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
 
-function logger(req, res, next) {}
+// custom middleware
+
+// Logs request method, request URL, and current date
+function logger(req, res, next) {
+  let date = new Date();
+
+  console.log(`Request method: ${req.method}`);
+  console.log(`Request URL: http://localhost:4000${req.url}`);
+  console.log(`Current date: ${date.toDateString()}`);
+  next();
+}
 
 module.exports = server;
